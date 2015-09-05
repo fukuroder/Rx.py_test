a7c38ad9b3f5df3b0cd9b2e4e20846c57e1040c2
fukuroda <fukuroder@live.jp>
2015-09-06 01:32:42 +0900

    追加

---
+++ test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from rx.subjects import Subject # pip install rx
+import json, operator, math
+
+if __name__ == "__main__":
+    # JSON読み込み
+    with open('test.json') as f:
+        j = json.load(f)
+
+    # モジュール作成
+    modules = { m['name']:Subject() for m in j['modules'] }
+
+    # モジュール配線
+    for m in filter(lambda m: m['module_type'] != 'const', j['modules']):
+        module_type = m['module_type'];
+        self_name = m['name']
+        input_names = m['inputs']
+        if   module_type == 'add':
+            modules[ input_names[0] ].zip(modules[ input_names[1] ], operator.add) \
+                .subscribe(modules[ self_name ].on_next)
+        elif module_type == 'sub':
+            modules[ input_names[0] ].zip(modules[ input_names[1] ], operator.sub) \
+                .subscribe(modules[ self_name ].on_next)
+        elif module_type == 'mul':
+            modules[ input_names[0] ].zip(modules[ input_names[1] ], operator.mul) \
+                .subscribe(modules[ self_name ].on_next)
+        elif module_type == 'div':
+            modules[ input_names[0] ].zip(modules[ input_names[1] ], operator.truediv) \
+                .subscribe(modules[ self_name ].on_next)
+        elif module_type == 'sin':
+            modules[ input_names[0] ].select(math.sin).subscribe(modules[ self_name ].on_next)
+        elif module_type == 'cos':
+            modules[ input_names[0] ].select(math.cos).subscribe(modules[ self_name ].on_next)
+        elif module_type == 'tan':
+            modules[ input_names[0] ].select(math.tan).subscribe(modules[ self_name ].on_next)
+        elif module_type == 'out':
+            modules[ input_names[0] ].subscribe(print) # 計算結果の表示
+
+    # 定数値を設定
+    for m in filter(lambda m: m['module_type'] == 'const', j['modules']):
+        self_name = m['name']
+        value = m['value']
+        modules[ self_name ].on_next(value)
---
+++ test_check.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import math
+if __name__ == '__main__':
+    sin1 = math.sin(1)
+    add1 = sin1+1
+    cos1 = math.cos(add1)
+    sub1 = sin1-cos1
+    mul1 = sub1*add1
+    div1 = cos1/2
+    add2 = mul1+div1
+    out = add2
+    print(out)
